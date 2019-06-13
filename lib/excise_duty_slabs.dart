3f64e1cca905d87849d09254e519800281f6f803
arindam <asarkar1990@gmail.com>
2019-06-13 15:50:27 +0530

    tltc primary build

---
+++ lib/excise_duty_slabs.dart
@@ -0,0 +1,44 @@
+Iterable slabs = [
+  {
+    "mrpFrom": 4001,
+    "mrpTo": 5000,
+    "factor": 0.92,
+    "dutyFrom": 1521,
+    "dutyTo": 1600
+  },
+  {
+    "mrpFrom": 5001,
+    "mrpTo": 6500,
+    "factor": 0.77,
+    "dutyFrom": 1601,
+    "dutyTo": 1750
+  },
+  {
+    "mrpFrom": 6501,
+    "mrpTo": 8500,
+    "factor": 0.64,
+    "dutyFrom": 1751,
+    "dutyTo": 1900
+  },
+  {
+    "mrpFrom": 8501,
+    "mrpTo": 12500,
+    "factor": 0.48,
+    "dutyFrom": 1901,
+    "dutyTo": 2100
+  },
+  {
+    "mrpFrom": 12501,
+    "mrpTo": 24000,
+    "factor": 0.29,
+    "dutyFrom": 2101,
+    "dutyTo": 2400
+  },
+  {
+    "mrpFrom": 24001,
+    "mrpTo": 48000,
+    "factor": 0.18,
+    "dutyFrom": 2401,
+    "dutyTo": 3000
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news/dice.dart';
+import 'package:flutter_news/tltc.dart';
 
 void main() {
   runApp(
-    MaterialApp(title: "My App", home: DicePage()),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My App",
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: TLTC(),
+    ),
   );
 }
 
@@ -17,7 +25,7 @@ class Home extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                margin: EdgeInsets.symmetric(horizontal: 12,vertical: 20),
+                margin: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
                 width: 100,
                 height: 100,
                 color: Colors.red,
@@ -35,7 +43,6 @@ class Home extends StatelessWidget {
                 color: Colors.orange,
                 child: Text('2'),
               ),
-
             ],
           ),
         ),
